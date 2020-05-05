4acd09b8f02c7032661e17e007beb9373fba5991
Kevin Boston <kbos22@gmail.com>
2020-05-05 01:01:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+  
+end
+
+
+def batch_badge_creator(names)
+  badge_messages = []
+  names.each do |name|
+    badge_messages << badge_maker(name)
+  end
+  badge_messages
+end
+
+
+def assign_rooms(names)
+  room = 1
+  
+end
